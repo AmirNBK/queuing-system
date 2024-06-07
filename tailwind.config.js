d9057374858d3dbde1ffc43cdb3b10f16a473542
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-06-07 21:18:16 +0330

    developed rating page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html" , "./queue.html"],
+  content: ["./index.html" , "./queue.html" , "./rating.html"],
 
   theme: {
     extend: {},
