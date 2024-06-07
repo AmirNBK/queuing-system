e6c1d6f23311d9cf985af1c794a9ad1ea7a36e5a
AmirHossein Norbakhsh <amircr7h@icloud.com>
2024-06-07 23:13:02 +0330

    developed the map locations page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html" , "./queue.html" , "./rating.html" , "./locations.html"],
+  content: ["./index.html" , "./queue.html" , "./rating.html" , "./locations.html" , "./map-locations.html"],
 
   theme: {
     extend: {},
