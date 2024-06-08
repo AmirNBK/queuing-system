f6ffec0181f96b7444171664c8c164dc3391b1da
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2024-06-08 15:37:39 +0330

    developed the single reward page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html" , "./queue.html" , "./queues-list.html" ,  "./rating.html" , "./locations.html" , "./map-locations.html" , "./queue-selection.html" , "./rewards.html"],
+  content: ["./index.html", "./queue.html", "./queues-list.html", "./rating.html", "./locations.html", "./map-locations.html", "./queue-selection.html", "./rewards.html", "./single-reward.html"],
 
   theme: {
     extend: {},
