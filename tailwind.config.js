6f6705e426d51b6d1fc0e34858dd44d4f4ea645a
Amir Hossein Nourbakhsh <nou2177@Tamco.Eng>
2024-06-08 14:45:59 +0330

    developed Queues list page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html" , "./queue.html" , "./rating.html" , "./locations.html" , "./map-locations.html" , "./queue-selection.html"],
+  content: ["./index.html" , "./queue.html" , "./queues-list.html" ,  "./rating.html" , "./locations.html" , "./map-locations.html" , "./queue-selection.html"],
 
   theme: {
     extend: {},
